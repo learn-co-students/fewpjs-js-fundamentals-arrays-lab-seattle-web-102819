d774c3c85e9bfb7135b30a460f5d022996987f9b
bahayg <bahayg@gmail.com>
2019-12-09 17:37:26 -0800

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-// import { appendFile } from "fs";
-
 // Write your solution here!
 
 const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
-const removeFirst = ["Milo", "Otis", "Garfield"]
+const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
 prepend.unshift("Odie");
 removeLast.pop();
 removeFirst.shift();
-
-
-// function appendItems() {
-//     let new_array = append.push("Odie");
-//     return append
-// }
-
-// console.log(appendItems())
-
-
-// function appendItem(arr) {
-//     let new_append = arr.push("Odie");
-//     return new_append
-// }
-// appendItem(append)
-// console.log(appendItem())
-
-//    function prepend() {
-//        prepend.unshift("Odie");
-//    }
-
-//    function removeLast() {
-//        removeLast.pop();
-//    }
-
-//    function removeFirst() {
-//         removeFirst.shift();
-
-//    }
